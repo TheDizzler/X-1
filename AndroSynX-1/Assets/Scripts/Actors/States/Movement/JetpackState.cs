30a4fa4909c85c0bad365520edd830e7c51716f7
TheDizzler <tgillon.music@gmail.com>
2021-03-01 18:02:05 +0900

    reorganizing

--- AndroSynX-1/Assets/Scripts/Actors/States/Movement/JetpackState.cs
+++ AndroSynX-1/Assets/Scripts/Actors/States/Movement/JetpackState.cs
@@ -4,12 +4,23 @@ namespace AtomosZ.AndroSyn.Actors.State
 {
 	public class JetpackState : MonoBehaviour, IMovementState
 	{
+		/// <summary>
+		/// How much energy is drained per second of jetpack operation
+		/// </summary>
+		[Tooltip("How much energy is drained per second of jetpack operation")]
+		[SerializeField] private int jetpackDrain = 5;
+		/// <summary>
+		/// How much energy is recharged per second when not in operation.
+		/// </summary>
+		[Tooltip("How much energy is recharged per second when not in operation.")]
+		[SerializeField] private int jetpackRecharge = 5;
 		[SerializeField] private int maxJetpackEnergy = 10;
 		public float currentEnergy = 10;
 		[SerializeField] private float jetpackPower = 3.3f;
 		/// <summary>
-		/// Time since used jetpack that starts recharging.
+		/// Time since used jetpack before recharging starts.
 		/// </summary>
+		[Tooltip("Time since used jetpack before recharging starts.")]
 		[SerializeField] private float rechargeDelay = 1f;
 		private float timeUntilRecharge = 0;
 
@@ -31,7 +42,6 @@ namespace AtomosZ.AndroSyn.Actors.State
 
 		public void EnterState(MovementStateType previousState)
 		{
-			//Debug.Log("Entering JetpackState");
 			jetpackOn = true;
 			Vector2 inputVelocity = Vector2.zero;
 			inputVelocity.y = actor.inputVelocity.y * jetpackPower;
@@ -40,7 +50,6 @@ namespace AtomosZ.AndroSyn.Actors.State
 
 		public MovementStateType ExitState(MovementStateType nextState)
 		{
-			//Debug.Log("Exiting JetpackState");
 			jetpackOn = false;
 			return movementStateType;
 		}
@@ -49,7 +58,7 @@ namespace AtomosZ.AndroSyn.Actors.State
 		{
 			if (jetpackOn)
 			{
-				currentEnergy -= 2 * Time.deltaTime;
+				currentEnergy -= jetpackDrain * Time.deltaTime;
 				if (currentEnergy <= 0)
 					currentEnergy = 0;
 			}
@@ -58,7 +67,7 @@ namespace AtomosZ.AndroSyn.Actors.State
 				timeUntilRecharge -= Time.deltaTime;
 				if (timeUntilRecharge <= 0)
 				{
-					currentEnergy += 2 * Time.deltaTime;
+					currentEnergy += jetpackRecharge * Time.deltaTime;
 					if (currentEnergy > maxJetpackEnergy)
 						currentEnergy = maxJetpackEnergy;
 				}
@@ -100,7 +109,6 @@ namespace AtomosZ.AndroSyn.Actors.State
 				inputVelocity.y = actor.inputVelocity.y * jetpackPower;
 				timeUntilRecharge = rechargeDelay;
 			}
-
 
 			actor.actorPhysics.desiredVelocity = inputVelocity;
 			return MovementStateType.NONE;

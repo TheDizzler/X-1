18de5a35928661350a0db2111caa64125c1533fe
TheDizzler <tgillon.music@gmail.com>
2021-03-08 13:35:04 +0900

    sci-fi door

---
+++ AndroSynX-1/Assets/Terrain/Door/Door.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AtomosZ.AndroSyn.Weapons
+{
+	public class Door : MonoBehaviour
+	{
+		public static readonly int IsOpenHash = Animator.StringToHash("isOpen");
+
+
+		private void OnCollisionEnter2D(Collision2D collision)
+		{
+			if (collision.collider.CompareTag(Tags.BULLET))
+			{
+				GetComponent<Animator>().SetBool(IsOpenHash, true);
+			}
+		}
+
+	}
+}
